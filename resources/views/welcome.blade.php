c8c1cbb2d3a55102f328c6b854149b1c8590ff63
Maria Iara <mariaiarasbraga@gmail.com>
2024-03-28 22:48:05 -0300

    iniciando o projeto - configurando Laravel - configrando Tailwind

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerador de Atas</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <header></header>
+    <main>
+        <section class="section1">
+            <div class="apresentation">
+                <div class="images">
+                    <img src="../img/ata.png" alt="Ata grande" class="img1">
+                    <img src="../img/ata.png" alt="Ata pequena" class="img2">
+                </div>
+                <div class="apresentation-text">
+                    <h1>Gerador de <span style="text-decoration: underline; text-decoration-color: rgb(99, 179, 211);">Atas</span></h1>
+                    <h2>A Ata perfeita não exist... <br>
+                        Quem disse? Este é o lugar ideal para criar as Atas de reuniões da sua equipe. É tão simples que parece mágica! &#10024
+                    </h2>
+                </div>
+            </div>
+            <div class="attributes">
+                <div class="icons">
+                    <img src="../img/icon.png" alt="">
+                    <h3>Prático</h3>
+                </div>
+                <div class="icons">
+                    <img src="../img/icon.png" alt="">
+                    <h3>Organizado</h3>
+                </div>
+                <div class="icons">
+                    <img src="../img/icon.png" alt="">
+                    <h3>Gratuito</h3>
+                </div>
+                <div class="icons">
+                    <img src="../img/icon.png" alt="">
+                    <h3>Rápido</h3>
+                </div>
+                <div class="icons">
+                    <img src="../img/icon.png" alt="">
+                    <h3>Seguro</h3>
+                </div>
+            </div>
+        </section>
+        <section class="section2">
+            <div class="init">
+                <div class="text-init">
+                    <img src="../img/pasta.png" alt="pasta icon">
+                    <h3>
+                        Cansado de perder tempo criando documentos? Estamos aqui para simplificar sua vida. <br>
+                        Em alguns minutos você cria um relatório personalizado para a sua empresa, e pode compartilhar com os membros de sua equipe aqui mesmo. E o melhor: a organização fica por nossa conta! 
+                    </h3>
+                </div>
+                <a href="#" class="button">COMEÇAR</a>    
+            </div>
+        </section>
+    </main>
+    <footer></footer>
+</body>
+</html>
